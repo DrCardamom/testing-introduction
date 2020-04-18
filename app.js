958a78514308ff9e4b7e87a8a6f517d5238b343c
DrCardamom <erkshimba@gmail.com>
2020-04-18 10:38:05 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { generateText, createElement, validateInput } = require('./util');
+const { createElement, checkAndGenerate } = require('./util');
 
 const initApp = () => {
   // Initializes the app, registers the button click listener
@@ -12,18 +12,30 @@ const addUser = () => {
   const newUserNameInput = document.querySelector('input#name');
   const newUserAgeInput = document.querySelector('input#age');
 
-  if (
-    !validateInput(newUserNameInput.value, true, false) ||
-    !validateInput(newUserAgeInput.value, false, true)
-  ) {
+  // // the below means 'if both of input fields are empty, dont make a li element'
+  // if (
+  //   !validateInput(newUserNameInput.value, true, false) ||
+  //   !validateInput(newUserAgeInput.value, false, true)
+  // ) {
+  //   return;
+  // }
+  //
+  // //But if at least one of them is filled, make a li element with the data
+  // const outputText = generateText(
+  //   newUserNameInput.value,
+  //   newUserAgeInput.value
+  // );
+
+  // Instead of above, you can do
+  if(!checkAndGenerate) {
     return;
   }
-
-  const userList = document.querySelector('.user-list');
-  const outputText = generateText(
+  const outputText = checkAndGenerate(
     newUserNameInput.value,
     newUserAgeInput.value
   );
+
+  const userList = document.querySelector('.user-list');
   const element = createElement('li', outputText, 'user-item');
   userList.appendChild(element);
 };
---
+++ util.test.js
@@ -0,0 +1,50 @@
+const puppeteer = require('puppeteer');
+const { generateText, checkAndGenerate } = require('./util');
+
+// Unit test against generateText()
+test('should output name and age', () => {
+   const text = generateText('Shimba', 28)
+   expect(text).toBe('Shimba (28 years old)')
+   const text2 = generateText('Roxi', 26)
+   expect(text2).toBe('Roxi (26 years old)')
+})
+
+// Another unit test against generateText()
+test('should output data-less text', () => {
+   const output = generateText('', null);
+   expect(output).toBe(' (null years old)');
+}) 
+
+
+// Integration test against checkAndGenerate()
+// It is integration test because checkAndGenerate() has 2 functions in it
+test('should generate a valid text output', () => {
+   const output = generateText('Shimba', 28)
+   expect(output).toBe('Shimba (28 years old)')
+})
+
+// E2E test using puppeteer
+test('should output the validated information on browser', async () => {
+   // Step1: Create a browser
+   const browser = await puppeteer.launch({
+      headless: false, 
+      slowMo: 30,
+      // args: ['--window-size=1920,1080']
+   })
+
+   // Step2: create 'page object'
+   const page = await browser.newPage();
+
+   // Step3: tell this test what to do
+   await page.goto('file:///home/shimba/js-testing-introduction/index.html')
+   await page.click('input#name')
+   await page.type('input#name', 'Anna')
+   await page.click('input#age')
+   await page.type('input#age', '28')
+   await page.click('#btnAddUser')
+
+   // Step4: you can also have your program check the output instead of you seeing it 
+   const finalText = await page.$eval('.user-item', el => el.textContent)
+   expect(finalText).toBe('Anna (28 years old)')
+
+}, 10000)
